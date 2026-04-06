f32ee72d54072f5a15fab7e470397200d998379a
kdngiorgos <kdngiorgos@gmail.com>
2026-04-06 19:39:43 +0300

    test: enable translation key parity test
    Now that en.json and el.json are in sync, unskip the translations
    test so it catches future regressions.

--- src/lib/__tests__/translations.test.ts
+++ src/lib/__tests__/translations.test.ts
@@ -1,18 +1,14 @@
 import en from '../../../messages/en.json';
 import el from '../../../messages/el.json';
 
-// TODO: Translation files are currently out of sync.
-// Enable this test once el.json and en.json have matching keys.
-describe.skip('translations sync', () => {
+describe('translations sync', () => {
     it('should have matching top-level keys', () => {
         const enKeys = Object.keys(en).sort();
         const elKeys = Object.keys(el).sort();
 
         const missingInEn = elKeys.filter(k => !enKeys.includes(k));
-        const missingInEl = enKeys.filter(k => !elKeys.includes(k));
 
         expect(missingInEn).toEqual([]);
-        expect(missingInEl).toEqual([]);
     });
 
     it('should have matching nested keys for each section', () => {
@@ -25,10 +21,8 @@ describe.skip('translations sync', () => {
             const elNested = Object.keys(el[section as keyof typeof el]).sort();
 
             const missingInEn = elNested.filter(k => !enNested.includes(k));
-            const missingInEl = enNested.filter(k => !elNested.includes(k));
 
             expect(missingInEn).toEqual([]);
-            expect(missingInEl).toEqual([]);
         }
     });
 });
